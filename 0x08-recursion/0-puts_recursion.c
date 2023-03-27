861727af9aa8570d1de7728e450147081d0423cd
tiptopy <108461135+tiptopy@users.noreply.github.com>
2023-03-27 10:49:09 +0300

    Add files via upload

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
-
 /**
- * _puts_recursion - prints a string, followed by a new line.
- * @s: pointer to the string to print
- *
- * Return: void
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0') /* base case */
-    {
-        putchar('\n');
-        return;
-    }
-    putchar(*s); /* print the current character */
-    _puts_recursion(s + 1); /* make a recursive call with the next character */
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
 }
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,1 +1,4 @@
-This is not empty
+All the task of recursion.
+Still on the issue of doing hard things
+Following alx framework
+By Bwave / Bright Oghor
